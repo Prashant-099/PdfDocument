da1e3158bcaff2747dccdf9fe3f30af5228b0c4a
prashant kharat <prashant00095@gmail.com>
2025-06-18 21:10:04 +0530

    Add project files.

---
+++ PdfDocument/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PdfDocument
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PdfDocument/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PdfDocument
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
